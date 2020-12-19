77b7317f3145ba7df4af77cf29c67d2d77c9f93c
lvyoshino <m13983920501@163.com>
2020-12-19 04:44:26 +0000

    写个封面QaQ

---
+++ finalexam game/main.cpp
@@ -0,0 +1,22 @@
+//开始界面，先做一个没啥意义
+//记住每次编辑之前先同步一下，以防万一
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char input[10];
+	// 
+	cout << "Please chose your character: \n";
+	cout << "(1)" << "Human Sandtiger\n";
+	cout << "(2)" << "Elf Longleaf\n";
+	cout << "(3)" << "Halfling Sam\n";
+	cout << "(4)" << "Dwarf Bupu\n";
+	cout << "(5)" << "Orc Smellbad\n";
+    cout << "Please enter 1 - 5:";
+	cin >> input;
+	// 2020/12/19 4：40
+
+}
