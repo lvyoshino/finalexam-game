245554ec41c7637ba2a2bae2b3e28bd60d65f703
lvyoshino <m13983920501@163.com>
2020-12-22 03:28:07 +0000

    还原

--- finalexam game/main.cpp
+++ finalexam game/main.cpp
@@ -1,20 +1,9 @@
 //开始界面，先做一个没啥意义
 //记住每次编辑之前先同步一下，以防万一
-//#include <cstdlib>
-//#include <iostream>
-//using namespace std;
+#include <cstdlib>
+#include <iostream>
 
-#include "FantasyGame.h"//include head file
-
-//define class.init function
-inline void role::init(int r, int a, float ac, int d, float dc, int h) {
-	race = r;
-	attack = a;
-	attackChance = ac;
-	defence = d;
-	defenceChance = dc;
-	health = h;
-}
+using namespace std;
 
 int main()
 {
@@ -26,27 +15,8 @@ int main()
 	cout << "(3)" << "Halfling Sam\n";
 	cout << "(4)" << "Dwarf Bupu\n";
 	cout << "(5)" << "Orc Smellbad\n";
-    cout << "Please enter 1 - 5:";
+	cout << "Please enter 1 - 5:";
 	cin >> input;
 	// 2020/12/19 4：40
 
-
-
-
-
-
-	/*
-	* Those is for test about combat.
-	*date 12/20 21.48
-	*/
-	//This is a test about class
-	
-	role Human, Elf;
-	Human.init(1, 30, 2 / 3, 20, 1 / 2, 60);//race,attack,attack chance,defence,defence chance,health
-	Elf.init(2, 40, 1.0, 10, 1 / 4, 40);
-	Human.printPos();
-	Human.test();
-	//roleCombatdata(); 
-	combatTest();
-return EXIT_SUCCESS;
 }
